a9540c73be838dc6dd95bd7d899476bf5222b690
Nathan Gisvold <nathangisvold@gmail.com>
2023-02-01 08:52:02 -0800

    Create Tor.sh
    This script first updates the package list and installs Tor. Then it starts the Tor service and sets the environment variables http_proxy and https_proxy to use Tor as a proxy. Finally, it tests the internet connection by using the curl command to fetch the contents of the Tor check page and grep the result for the word "Congratulations".

---
+++ Tor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Update the package list and install Tor
+sudo apt-get update
+sudo apt-get install tor -y
+
+# Start the Tor service
+sudo service tor start
+
+# Set the environment variable for http_proxy and https_proxy to use Tor as a proxy
+export http_proxy="socks5://127.0.0.1:9050"
+export https_proxy="socks5://127.0.0.1:9050"
+
+# Test internet connection using curl command
+curl --silent https://check.torproject.org/ | grep Congratulations
